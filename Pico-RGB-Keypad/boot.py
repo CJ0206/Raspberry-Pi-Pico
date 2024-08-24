c5bc4cb3b003857cab4fa28589bb4912cfce73bb
CJ <CJ0206@users.noreply.github.com>
2024-08-24 18:36:31 +0100

    Update boot.py

--- Pico-RGB-Keypad/boot.py
+++ Pico-RGB-Keypad/boot.py
@@ -3,7 +3,7 @@ import storage
 import usb_hid
 
 # Set Manufacturer / Product / VID / PID
-supervisor.set_usb_identification(manufacturer='CJ', product='CJs Macro Pad', vid=0xC107, pid=0x1316)
+supervisor.set_usb_identification(manufacturer='CJ', product='CJs Macro Pad', vid=0xC116, pid=0x1316)
 # Disable CIRCUITPY / appearing as USB drive
 storage.disable_usb_drive()  
 # Set interface name for the gamepad
